79c6089fa9c850767e6b10e213327ea06b722d6f
Josh Long <josh@joshlong.com>
2018-10-19 12:18:01 +0300

    got fake data in the chart.

--- client/src/main/java/bootfx/StockController.java
+++ client/src/main/java/bootfx/StockController.java
@@ -25,38 +25,31 @@ public class StockController {
 
 	private final ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
 
-	private final Runnable runnable = new Runnable() {
+	private final Collection<String> stocks = Arrays.asList(
+		"ORCL", "PVTL", "MSFT", "GOOG", "AWS");
 
-		private final Collection<String> stocks = Arrays.asList(
-			"ORCL", "PVTL", "MSFT", "GOOG", "AWS");
-
-		private final Map<String, XYChart.Series<String, Number>> data =
-			stocks
-				.stream()
-				.collect(Collectors.toMap(k -> k, k -> new XYChart.Series<>(FXCollections.observableArrayList())));
-
-		private void contributeNewPriceToTicker(String ticker) {
-			Runnable run = () -> {
-				var price = ThreadLocalRandom.current().nextDouble(1000);
-				var dataPoint = new XYChart.Data<String, Number>(ticker, price);
-				this.data.get(ticker).getData().add(dataPoint);
-			};
-			Platform.runLater(run);
-		}
-
-		@Override
-		public void run() {
-			List<XYChart.Series<String, Number>> values = new ArrayList<>(this.data.values());
-			Platform.runLater(() -> stockticker.setData(FXCollections.observableList(values)));
-			this.stocks.forEach(this::contributeNewPriceToTicker);
-		}
-
+	private final Map<String, XYChart.Series<String, Number>> data =
+		stocks
+			.stream()
+			.collect(Collectors.toMap(k -> k, k -> new XYChart.Series<>(FXCollections.observableArrayList())));
+	private final Runnable runnable = () -> {
+		List<XYChart.Series<String, Number>> values =
+			new ArrayList<>(data.values());
+		Platform.runLater(() -> stockticker.setData(FXCollections.observableList(values)));
+		stocks.forEach(this::contributeNewPriceToTicker);
 	};
 
-	@FXML
-	public void initialize() throws InterruptedException {
-		log.info("calling FXML initialize()");
+	private void contributeNewPriceToTicker(String ticker) {
+		Runnable run = () -> {
+			var price = ThreadLocalRandom.current().nextDouble(1000);
+			var dataPoint = new XYChart.Data<String, Number>(ticker, price);
+			this.data.get(ticker).getData().add(dataPoint);
+		};
+		Platform.runLater(run);
+	}
 
+	@FXML
+	public void initialize() {
 		this.executor.scheduleAtFixedRate(this.runnable, 0, 1000,
 			TimeUnit.MILLISECONDS);
 	}
