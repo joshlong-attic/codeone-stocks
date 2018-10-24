28bf88322248d67e742dc59b543365bb0dff8dff
Josh Long <josh@joshlong.com>
2018-10-23 20:12:32 -0700

    polish

---
+++ ui/src/main/java/bootfx/JavaFxApplication.java
@@ -0,0 +1,43 @@
+package bootfx;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.stage.Stage;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Log4j2
+@SpringBootApplication
+public class JavaFxApplication extends Application {
+
+	@Bean
+	WebClient client() {
+		return WebClient.builder().build();
+	}
+
+	private ConfigurableApplicationContext context;
+
+	@Override
+	public void init() {
+		this.context = new SpringApplicationBuilder()
+			.sources(JavaFxApplication.class)
+			.run();
+	}
+
+	@Override
+	public void start(Stage stage) {
+		this.context.publishEvent(new StageReadyEvent(stage));
+	}
+
+	@Override
+	public void stop() {
+		this.context.close();
+		Platform.exit();
+	}
+
+}
+
---
+++ ui/src/main/java/bootfx/StageReadyEvent.java
@@ -0,0 +1,18 @@
+package bootfx;
+
+import javafx.stage.Stage;
+import org.springframework.context.ApplicationEvent;
+
+class StageReadyEvent extends ApplicationEvent {
+
+	private final Stage stage;
+
+	StageReadyEvent(Stage stage) {
+		super(stage);
+		this.stage = stage;
+	}
+
+	public Stage getStage() {
+		return stage;
+	}
+}
